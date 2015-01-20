4d622751019bb5bb1a137da1665a461784131057
Pelayo <parejo_p@epitech.eu>
2015-01-20 20:30:40 +0100

    This commit essentialy erases all changes done in the previous commit. This is the good one

---
+++ Source/MagUs/Public/MagUsAICharacter.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "MagUsAICharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGUS_API AMagUsAICharacter : public ACharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
