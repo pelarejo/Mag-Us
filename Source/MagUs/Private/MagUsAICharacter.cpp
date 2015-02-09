80ca4ae6134aa04544a2c3c7c02b92e8da6ced40
Remi Lesaffre <remi.lesaffre@epitech.eu>
2015-02-09 09:49:22 +0100

    Added AI death signal for the player

--- Source/MagUs/Private/MagUsAICharacter.cpp
+++ Source/MagUs/Private/MagUsAICharacter.cpp
@@ -2,6 +2,7 @@
 
 #include "MagUs.h"
 #include "MagUsAICharacter.h"
+#include "MagUsCharacter.h"
 #include "Engine.h"
 
 void AMagUsAICharacter::ApplyDamageMomentum(float DamageTaken, FDamageEvent const& DamageEvent, APawn* PawnInstigator, AActor* DamageCauser)
@@ -18,6 +19,12 @@ void AMagUsAICharacter::ApplyDamageMomentum(float DamageTaken, FDamageEvent cons
 		{
 			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("" + this->GetName() + " died"));
 		}
+
+		/** Check if the Instigator is the player then signal an AI is killed */
+		AMagUsCharacter *Player(Cast<AMagUsCharacter>(PawnInstigator));
+		if (Player)
+			Player->Killed(this);
+
 		Destroy();
 	}
 }
